6a6b4e3294132980dbe8db643dcd78d05c324a60
BFalberg <benjaminfalberg@gmail.com>
2025-11-21 10:40:52 +0100

    refactor: improve CSS formatting and rename Switch component to Toggle

--- frontend/app/components/ContextMenu.tsx
+++ frontend/app/components/ContextMenu.tsx
@@ -2,5 +2,9 @@
 type ContextVariant = "profile" | "post";
 
 export default function ContextMenu({ children }: { children: any }) {
-  return <section className="glass-card bg-neutral-grey text-white rounded-3xl max-w-[240px] p-2 flex flex-col gap-1">{children}</section>;
+  return (
+    <section className="glass-card bg-neutral-grey text-white rounded-3xl max-w-60 p-2 flex flex-col gap-1">
+      {children}
+    </section>
+  );
 }
--- frontend/app/components/Switch.tsx
+++ frontend/app/components/Switch.tsx
@@ -1,8 +1,14 @@
-export default function Switch({ value, onClick }: { value: boolean; onClick: () => void }) {
+export default function Toggle({
+  value,
+  onClick,
+}: {
+  value: boolean;
+  onClick: () => void;
+}) {
   return (
-    <label className="switch relative inline-block w-[56px] h-[28px]">
+    <label className="toggle relative inline-block w-14 h-7">
       <input type="checkbox" className="opacity-0 w-0 h-0" />
-      <span className="slider absolute cursor-pointer top-0 left-0 right-0 bottom-0 transition-all rounded-[34px] duration-100 ease-in-out"></span>
+      <span className="toggle-switch absolute cursor-pointer top-0 left-0 right-0 bottom-0 transition-all rounded-[34px] duration-100 ease-in-out"></span>
     </label>
   );
 }
