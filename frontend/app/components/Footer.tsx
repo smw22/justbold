8b00075d25733107be458f9d00974f16f1fef72b
smw22 <sergiomoronwestergaard@gmail.com>
2025-11-21 10:28:22 +0100

    Refactor: Footer component to use NavLink for navigation; Custom Header depending on route.

--- frontend/app/components/Footer.tsx
+++ frontend/app/components/Footer.tsx
@@ -1,24 +1,80 @@
 import Icon from "./icon";
+import { NavLink } from "react-router";
 
 export default function Footer() {
   return (
-    <footer className="bg-black">
+    <footer className="bg-black fixed bottom-5 left-[5%] w-[90%] py-4 text-white rounded-full">
       <nav>
         <ul className="flex justify-evenly">
           <li>
-            <Icon name="HomeSimpleDoor" />
+            <NavLink
+              to="/"
+              className={({ isActive, isPending }) =>
+                isPending
+                  ? "flex flex-col items-center text-sm"
+                  : isActive
+                    ? "bg-gray-500 rounded-full flex flex-col items-center text-sm"
+                    : "flex flex-col items-center text-sm"
+              }
+            >
+              <Icon name="HomeSimpleDoor" size={24} /> Home
+            </NavLink>
           </li>
           <li>
-            <Icon name="SmallShopAlt" />
+            <NavLink
+              to="/services"
+              className={({ isActive, isPending }) =>
+                isPending
+                  ? "flex flex-col items-center text-sm"
+                  : isActive
+                    ? "bg-gray-500 rounded-full flex flex-col items-center text-sm"
+                    : "flex flex-col items-center text-sm"
+              }
+            >
+              <Icon name="SmallShopAlt" size={24} /> Services
+            </NavLink>
           </li>
           <li>
-            <Icon name="AddCircle" />
+            <NavLink
+              to="/create"
+              className={({ isActive, isPending }) =>
+                isPending
+                  ? "flex flex-col items-center text-sm"
+                  : isActive
+                    ? "bg-gray-500 rounded-full flex flex-col items-center text-sm"
+                    : "flex flex-col items-center text-sm"
+              }
+            >
+              <Icon name="AddCircle" size={24} /> Create
+            </NavLink>
           </li>
           <li>
-            <Icon name="ChatLines" />
+            <NavLink
+              to="/chats"
+              className={({ isActive, isPending }) =>
+                isPending
+                  ? "flex flex-col items-center text-sm"
+                  : isActive
+                    ? "bg-gray-500 rounded-full flex flex-col items-center text-sm"
+                    : "flex flex-col items-center text-sm"
+              }
+            >
+              <Icon name="ChatLines" size={24} /> Chats
+            </NavLink>
           </li>
           <li>
-            <Icon name="UserCircle" />
+            <NavLink
+              to="/profile/:profileId"
+              className={({ isActive, isPending }) =>
+                isPending
+                  ? "flex flex-col items-center text-sm"
+                  : isActive
+                    ? "bg-gray-500 rounded-full flex flex-col items-center text-sm"
+                    : "flex flex-col items-center text-sm"
+              }
+            >
+              <Icon name="UserCircle" size={24} /> Profile
+            </NavLink>
           </li>
         </ul>
       </nav>
