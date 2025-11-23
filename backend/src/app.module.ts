941ceee755305ecd73d7945ef8c7e6fc7a488173
BFalberg <benjaminfalberg@gmail.com>
2025-11-23 20:56:08 +0100

    feat: implement Posts module with CRUD operations

--- backend/src/app.module.ts
+++ backend/src/app.module.ts
@@ -5,6 +5,8 @@ import { TypeOrmModule } from "@nestjs/typeorm";
 import { ConfigModule } from "@nestjs/config";
 import { UsersModule } from "./users/users.module";
 import { User } from "./users/entities/user.entity";
+import { PostsModule } from "./posts/posts.module";
+import { Post } from "./posts/entities/post.entity";
 
 @Module({
   imports: [
@@ -16,10 +18,11 @@ import { User } from "./users/entities/user.entity";
       username: process.env.DB_USER,
       password: process.env.DB_PASSWORD,
       database: process.env.DB_NAME,
-      entities: [User],
+      entities: [User, Post],
       synchronize: true,
     }),
     UsersModule,
+    PostsModule,
   ],
   controllers: [AppController],
   providers: [AppService],
---
+++ backend/src/posts/entities/post.entity.ts
@@ -0,0 +1,29 @@
+import { Entity, Column, PrimaryGeneratedColumn, ManyToOne } from "typeorm";
+import { User } from "../../users/entities/user.entity";
+
+@Entity()
+export class Post {
+  @PrimaryGeneratedColumn("uuid")
+  id: string;
+
+  @Column()
+  title: string;
+
+  @Column("text")
+  content: string;
+
+  @Column()
+  tags: string;
+
+  @Column()
+  media: string;
+
+  //   @ManyToOne(() => User, (user) => user.posts, { onDelete: "CASCADE" })
+  //   user: User;
+
+  @Column()
+  user_id: string;
+
+  @Column({ type: "timestamp", default: () => "CURRENT_TIMESTAMP" })
+  created: Date;
+}
