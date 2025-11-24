d87277d2c4283ec5f4f73ee5b395557a056b4ef1
BFalberg <benjaminfalberg@gmail.com>
2025-11-24 09:36:38 +0100

    create questions

---
+++ backend/src/questions/entities/question.entity.ts
@@ -0,0 +1,29 @@
+import {
+  Entity,
+  PrimaryGeneratedColumn,
+  Column,
+  CreateDateColumn,
+  ManyToOne,
+  JoinColumn,
+} from "typeorm";
+import { User } from "../../users/entities/user.entity";
+
+@Entity()
+export class Question {
+  @PrimaryGeneratedColumn("uuid")
+  id: string;
+
+  // The user who has got the question
+  @ManyToOne(() => User, { eager: true })
+  @JoinColumn({ name: "user_id" })
+  user: User;
+
+  @Column("text")
+  question: string;
+
+  @Column("text", { nullable: true })
+  answer: string;
+
+  @CreateDateColumn()
+  created: Date;
+}
