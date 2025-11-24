8551e584adcc6fcedb50cc30a0390798f69ec5ea
BFalberg <benjaminfalberg@gmail.com>
2025-11-24 10:59:21 +0100

    create collaborations endpoints + small typo fix of posts

--- backend/src/collaborations/dto/update-collaboration.dto.ts
+++ backend/src/collaborations/dto/update-collaboration.dto.ts
@@ -1,4 +1,35 @@
-import { PartialType } from '@nestjs/mapped-types';
-import { CreateCollaborationDto } from './create-collaboration.dto';
+import {
+  IsString,
+  IsOptional,
+  IsBoolean,
+  IsArray,
+  IsUUID,
+} from "class-validator";
 
-export class UpdateCollaborationDto extends PartialType(CreateCollaborationDto) {}
+export class UpdateCollaborationDto {
+  @IsOptional()
+  @IsString()
+  media?: string;
+
+  @IsString()
+  title: string;
+
+  @IsString()
+  content: string;
+
+  @IsArray()
+  @IsUUID("4", { each: true })
+  genreIds: string[];
+
+  @IsOptional()
+  @IsBoolean()
+  paid?: boolean;
+
+  @IsOptional()
+  @IsString()
+  location?: string;
+
+  @IsArray()
+  @IsUUID("4", { each: true })
+  tagIds: string[];
+}
