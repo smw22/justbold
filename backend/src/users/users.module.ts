b059bfa3211b8d4601bea14390f2cbbde1609561
BFalberg <benjaminfalberg@gmail.com>
2025-11-25 13:50:26 +0100

    create endpoint to get posts of a specific user

--- backend/src/users/users.module.ts
+++ backend/src/users/users.module.ts
@@ -1,8 +1,13 @@
-import { Module } from '@nestjs/common';
-import { UsersService } from './users.service';
-import { UsersController } from './users.controller';
+import { Module } from "@nestjs/common";
+import { UsersService } from "./users.service";
+import { UsersController } from "./users.controller";
+import { TypeOrmModule } from "@nestjs/typeorm";
+
+import { User } from "./entities/user.entity";
+import { Post } from "../posts/entities/post.entity";
 
 @Module({
+  imports: [TypeOrmModule.forFeature([User, Post])],
   controllers: [UsersController],
   providers: [UsersService],
 })
