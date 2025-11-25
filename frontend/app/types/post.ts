e73cd1a077b33a0ef686130fd944a48eb1ff77d8
Jacob Thorwest <jacob-jaytee@hotmail.com>
2025-11-25 14:37:17 +0100

    Post and Profile types have been defined and put in folder "types". Sheeeesh I'm legendary.

---
+++ frontend/app/types/post.ts
@@ -0,0 +1,7 @@
+export interface PostType {
+  title: string;
+  content: string;
+  tags: string[];
+  media: string;
+  created: Date;
+}
---
+++ frontend/app/types/profile.ts
@@ -0,0 +1,7 @@
+export interface ProfileType {
+  title: string;
+  content: string;
+  tags: string[];
+  media: string;
+  created: Date;
+}
