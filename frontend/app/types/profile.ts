fd17b62df2e2c7591d42b449f8d092f0f4a097a4
Jacob Thorwest <jacob-jaytee@hotmail.com>
2025-11-25 14:41:32 +0100

    Wait no, I suck. Here's the ACTUAL profile type.

--- frontend/app/types/profile.ts
+++ frontend/app/types/profile.ts
@@ -1,7 +1,24 @@
 export interface ProfileType {
-  title: string;
-  content: string;
-  tags: string[];
-  media: string;
+  name: string;
+  email: string;
+  phone: string;
+  year_of_birth: number;
+  bio: string;
+  about: string;
+  location: string;
+  theme: string;
+  connections: string[];
+  instagram: string;
+  twitter: string;
+  facebook: string;
+  youtube: string;
+  tiktok: string;
+  spotify_embed_link: string;
+  videos: string[];
+  looking_for: string[];
+  subscription: string;
+  user_type: string;
+  genres: string[];
+  profile_image: string;
   created: Date;
 }
