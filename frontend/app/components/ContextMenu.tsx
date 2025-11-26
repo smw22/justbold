0cfdde6da9786993612317874584aff616337d08
Jacob Thorwest <jacob-jaytee@hotmail.com>
2025-11-26 13:40:49 +0100

    ContextMenu can now be closed when clicking outside of it. Awesome UX!

--- frontend/app/components/ContextMenu.tsx
+++ frontend/app/components/ContextMenu.tsx
@@ -1,9 +1,22 @@
+import { useRef, useEffect } from "react";
+
 // Using a type union (recommended for simple cases)
 type ContextVariant = "profile" | "post";
 
-export default function ContextMenu({ show, children }: { show: boolean; children: any }) {
+export default function ContextMenu({ show, setShow, children }: { show: boolean; setShow: (e: boolean) => void; children: any }) {
+  const menuRef = useRef<HTMLDivElement>(null);
+
+  useEffect(() => {
+    function handleClickOutside(event: MouseEvent) {
+      if (menuRef.current && !menuRef.current.contains(event.target as Node)) {
+        setShow(false);
+      }
+    }
+    document.addEventListener("mousedown", handleClickOutside);
+  });
+
   return (
-    <div className="absolute top-10 right-0">
+    <div ref={menuRef} className={`${show ? "absolute" : "hidden"} top-10 right-0 display`}>
       <div className="glass-card bg-neutral-grey text-white rounded-3xl min-w-60 max-w-60 p-2 flex flex-col gap-1">{children}</div>
     </div>
   );
