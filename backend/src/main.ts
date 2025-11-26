1d71acfc6c67934446fcaa5e029270b5fc81466d
Sergio Morón Westergaard <sergiomoronwestergaard@gmail.com>
2025-11-26 19:18:30 +0100

    feat: create services endpoints and added validation pipe for error handeling

--- backend/src/main.ts
+++ backend/src/main.ts
@@ -1,9 +1,13 @@
 import { NestFactory } from "@nestjs/core";
 import { AppModule } from "./app.module";
+// import { ValidationPipe } from "@nestjs/common";
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   app.setGlobalPrefix("api");
+
+  // Use validation pipe globally with whitelist option to ensure validation on dto fields
+  app.useGlobalPipes(new ValidationPipe({ whitelist: true }));
 
   // Enable CORS for all origins while in development
   app.enableCors();
