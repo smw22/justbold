2db0a0189b67a7229d4374045ddbddf8346ce272
Jacob Thorwest <jacob-jaytee@hotmail.com>
2025-11-26 13:28:27 +0100

    Changes to ContextMenu, so it can shift between showing and hidden

--- frontend/app/components/ContextMenu.tsx
+++ frontend/app/components/ContextMenu.tsx
@@ -1,10 +1,10 @@
 // Using a type union (recommended for simple cases)
 type ContextVariant = "profile" | "post";
 
-export default function ContextMenu({ children }: { children: any }) {
+export default function ContextMenu({ show, children }: { show: boolean; children: any }) {
   return (
-    <section className="glass-card bg-neutral-grey text-white rounded-3xl max-w-60 p-2 flex flex-col gap-1">
-      {children}
-    </section>
+    <div className="absolute top-10 right-0">
+      <div className="glass-card bg-neutral-grey text-white rounded-3xl min-w-60 max-w-60 p-2 flex flex-col gap-1">{children}</div>
+    </div>
   );
 }
