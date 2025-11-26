8f73dfcc352d2623fe29c0d10e77dbae80012215
BFalberg <benjaminfalberg@gmail.com>
2025-11-26 11:46:54 +0100

    create filter container

--- frontend/app/routes/collaborations/components/CollaborationsFilter.tsx
+++ frontend/app/routes/collaborations/components/CollaborationsFilter.tsx
@@ -1,17 +1,43 @@
+import { useState } from "react";
 import Icon from "~/components/icon";
 
-export default function CollaborationsFilter() {
+function CollabFilterContainer() {
   return (
-    <div className="flex items-center gap-2">
-      <input
-        type="text"
-        placeholder="Search collaborations..."
-        className="w-full p-2 rounded-lg bg-neutral-200 outline-primary-yellow"
-      />
-      <button className="flex items-center gap-1">
-        <Icon name="Filter" size={24} />
-        <span>Filter</span>
-      </button>
+    <div className="bg-white rounded-3xl p-4 border border-black/10 flex flex-col gap-2">
+      <div>
+        <h4>Sort By</h4>
+      </div>
+      <div>
+        <h4>Role</h4>
+      </div>
+      <div>
+        <h4>Genre</h4>
+      </div>
+    </div>
+  );
+}
+
+export default function CollaborationsFilter() {
+  const [filterOpen, setFilterOpen] = useState(true);
+
+  const toggleFilter = () => {
+    setFilterOpen(!filterOpen);
+  };
+
+  return (
+    <div className="flex flex-col gap-4">
+      <div className="flex items-center gap-2">
+        <input
+          type="text"
+          placeholder="Search collaborations..."
+          className="w-full p-2 rounded-lg bg-neutral-200 outline-primary-yellow"
+        />
+        <button className="flex items-center gap-1" onClick={toggleFilter}>
+          <Icon name="Filter" size={24} />
+          <span>Filter</span>
+        </button>
+      </div>
+      {filterOpen && <CollabFilterContainer />}
     </div>
   );
 }
