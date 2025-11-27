9625b1b7e816e7aac3364039ffac11e78b51fd4a
BFalberg <benjaminfalberg@gmail.com>
2025-11-27 13:39:15 +0100

    add auth service

--- backend/src/app.module.ts
+++ backend/src/app.module.ts
@@ -33,6 +33,7 @@ import { CommentsModule } from "./comments/comments.module";
 import { Comment } from "./comments/entities/comment.entity";
 import { LikesModule } from "./likes/likes.module";
 import { Like } from "./likes/entities/like.entity";
+import { AuthModule } from './auth/auth.module';
 
 @Module({
   imports: [
@@ -78,6 +79,7 @@ import { Like } from "./likes/entities/like.entity";
     QuestionsModule,
     CommentsModule,
     LikesModule,
+    AuthModule,
   ],
   controllers: [AppController],
   providers: [AppService],
