3946418d179a9432c8209316aac89afa90cedaed
smw22 <sergiomoronwestergaard@gmail.com>
2025-11-27 12:25:48 +0100

    refactor: enhance tag DTOs and entity with validation and relations

--- backend/src/tags/entities/tag.entity.ts
+++ backend/src/tags/entities/tag.entity.ts
@@ -1,10 +1,29 @@
-import { Entity, PrimaryGeneratedColumn, Column } from "typeorm";
+import {
+  Entity,
+  PrimaryGeneratedColumn,
+  Column,
+  OneToMany,
+  ManyToMany,
+} from "typeorm";
+import { Service } from "../../services/entities/service.entity";
+import { Post } from "../../posts/entities/post.entity";
+import { Collaboration } from "../../collaborations/entities/collaboration.entity";
 
 @Entity()
 export class Tag {
   @PrimaryGeneratedColumn("uuid")
   id: string;
 
-  @Column({ unique: true, length: 50 })
+  @Column({ type: "varchar", unique: true, length: 50 })
   title: string; // "recording", "mixing", etc.
+
+  // Relations
+  @OneToMany(() => Service, (service) => service.tag)
+  services: Service[];
+
+  @ManyToMany(() => Post, (post) => post.tags)
+  posts: Post[];
+
+  @ManyToMany(() => Collaboration, (collaboration) => collaboration.tags)
+  collaborations: Collaboration[];
 }
