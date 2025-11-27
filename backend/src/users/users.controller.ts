5ad57a6767c3208ff09c6f8b12908bd6b7407892
Jacob Thorwest <jacob-jaytee@hotmail.com>
2025-11-27 13:12:04 +0100

    get user reviews

--- backend/src/users/users.controller.ts
+++ backend/src/users/users.controller.ts
@@ -1,12 +1,4 @@
-import {
-  Controller,
-  Get,
-  Post,
-  Body,
-  Patch,
-  Param,
-  Delete,
-} from "@nestjs/common";
+import { Controller, Get, Post, Body, Patch, Param, Delete } from "@nestjs/common";
 import { UsersService } from "./users.service";
 import { CreateUserDto } from "./dto/create-user.dto";
 import { UpdateUserDto } from "./dto/update-user.dto";
@@ -78,5 +70,16 @@ export class UsersController {
         message: error.message,
       };
     }
+  }
+
+  @Get(":id/reviews")
+  async findAllReviews(@Param("id") id: string) {
+    const { data, avg_rating } = await this.usersService.findUserReviews(id);
+    return {
+      success: true,
+      avg_rating,
+      data,
+      message: "User reviews retrieved successfully",
+    };
   }
 }
