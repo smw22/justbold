03c146ba8df536b296df8b4dc3419931ab8cc691
Jacob Thorwest <jacob-jaytee@hotmail.com>
2025-11-28 09:39:24 +0100

    added error handling for reviews & questions route

--- backend/src/users/users.controller.ts
+++ backend/src/users/users.controller.ts
@@ -74,17 +74,36 @@ export class UsersController {
 
   @Get(":id/reviews")
   async findUserReviews(@Param("id") id: string) {
-    const { data, avg_rating } = await this.usersService.findUserReviews(id);
-    return {
-      success: true,
-      avg_rating,
-      data,
-      message: "User reviews retrieved successfully",
-    };
+    try {
+      const { data, avg_rating } = await this.usersService.findUserReviews(id);
+      return {
+        success: true,
+        avg_rating,
+        data,
+        message: "User reviews retrieved successfully",
+      };
+    } catch (error) {
+      return {
+        success: false,
+        message: error.message,
+      };
+    }
   }
 
   @Get(":id/questions")
   async findUserQuestions(@Param("id") id: string) {
-    return this.usersService.findUserQuestions(id);
+    try {
+      const data = await this.usersService.findUserQuestions(id);
+      return {
+        success: true,
+        data,
+        message: "User questions retrieved successfully",
+      };
+    } catch (error) {
+      return {
+        success: false,
+        message: error.message,
+      };
+    }
   }
 }
