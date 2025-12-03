89f75d05c60782b8eb9a2649448c0687e56ae4e3
Jacob Thorwest <jacob-jaytee@hotmail.com>
2025-12-03 09:13:21 +0100

    Notifications page UI has been designed
    We still need functionality of course.

--- frontend/app/components/Header.tsx
+++ frontend/app/components/Header.tsx
@@ -1,6 +1,6 @@
 import Icon from "./icon";
 import Logo from "../assets/icons/artwork/Logo";
-import { useLocation, useNavigate } from "react-router";
+import { useLocation, useNavigate, Link } from "react-router";
 
 export default function Header() {
   const location = useLocation();
@@ -9,24 +9,38 @@ export default function Header() {
 
   return (
     <header className="outer-wrapper">
-      <nav className="flex justify-between items-center px-4 py-6">
+      <nav className="flex justify-between items-center p-4">
         {/* navigate(-1) : Lets you go back to the previous page  */}
         {isHomePage ? (
           <Logo />
         ) : (
-          <button onClick={() => navigate(-1)} aria-label="Go back">
+          <button
+            onClick={() => navigate(-1)}
+            aria-label="Go back"
+            className="hover:bg-gray-200 p-2 rounded-full transition-colors duration-200 ease-in-out cursor-pointer"
+          >
             <Icon name="NavArrowLeft" color="black" size={20} />
           </button>
         )}
-        <ul className="flex gap-4">
-          <li>
-            <Icon name="Search" color="black" size={20} />
+        <ul className="flex gap-0">
+          <li className="hover:bg-gray-200 p-2 rounded-full transition-colors duration-200 ease-in-out cursor-pointer">
+            <Link to="/search">
+              <Icon name="Search" color="black" size={20} />
+            </Link>
+          </li>
+          <li className="hover:bg-gray-200 p-2 rounded-full transition-colors duration-200 ease-in-out cursor-pointer">
+            <Link to="/notifications">
+              <Icon name="BellNotification" color="black" size={20} />
+            </Link>
           </li>
           <li>
-            <Icon name="BellNotification" color="black" size={20} />
-          </li>
-          <li>
-            <Icon name="Menu" color="black" size={20} />
+            <button
+              onClick={() => alert("Open burger menu")}
+              className="hover:bg-gray-200 p-2 rounded-full transition-colors duration-200 ease-in-out cursor-pointer"
+              aria-label="Open side menu"
+            >
+              <Icon name="Menu" color="black" size={20} />
+            </button>
           </li>
         </ul>
       </nav>
