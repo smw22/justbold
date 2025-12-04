38884b0623a9fa32097e4cfd98375d9142e9fece
BFalberg <benjaminfalberg@gmail.com>
2025-12-04 12:11:20 +0100

    update skill endpoints

--- backend/src/skills/skills.service.ts
+++ backend/src/skills/skills.service.ts
@@ -1,26 +1,60 @@
-import { Injectable } from '@nestjs/common';
-import { CreateSkillDto } from './dto/create-skill.dto';
-import { UpdateSkillDto } from './dto/update-skill.dto';
+import { Injectable, HttpException } from "@nestjs/common";
+import { InjectRepository } from "@nestjs/typeorm";
+import { Repository } from "typeorm";
+import { Skill } from "./entities/skill.entity";
+import { CreateSkillDto } from "./dto/create-skill.dto";
+import { UpdateSkillDto } from "./dto/update-skill.dto";
 
 @Injectable()
 export class SkillsService {
-  create(createSkillDto: CreateSkillDto) {
-    return 'This action adds a new skill';
+  constructor(
+    @InjectRepository(Skill)
+    private skillsRepository: Repository<Skill>
+  ) {}
+
+  async create(createSkillDto: CreateSkillDto): Promise<Skill> {
+    const existingSkill = await this.skillsRepository.findOneBy({
+      title: createSkillDto.title,
+    });
+    if (existingSkill) {
+      throw new HttpException("Skill already exists", 400);
+    }
+    const skill = this.skillsRepository.create(createSkillDto);
+    return this.skillsRepository.save(skill);
   }
 
-  findAll() {
-    return `This action returns all skills`;
+  async findAll(): Promise<Skill[]> {
+    return this.skillsRepository.find();
   }
 
-  findOne(id: number) {
-    return `This action returns a #${id} skill`;
+  async findOne(title: string): Promise<Skill> {
+    const skillData = await this.skillsRepository.findOneBy({
+      title: title,
+    });
+    if (!skillData) {
+      throw new HttpException("Skill not found", 404);
+    }
+    return skillData;
   }
 
-  update(id: number, updateSkillDto: UpdateSkillDto) {
-    return `This action updates a #${id} skill`;
+  async update(title: string, updateSkillDto: UpdateSkillDto): Promise<Skill> {
+    const existingSkill = await this.skillsRepository.findOneBy({
+      title: title,
+    });
+    if (!existingSkill) {
+      throw new HttpException("Skill not found", 404);
+    }
+    const skillData = this.skillsRepository.merge(existingSkill, updateSkillDto);
+    return this.skillsRepository.save(skillData);
   }
 
-  remove(id: number) {
-    return `This action removes a #${id} skill`;
+  async remove(title: string): Promise<Skill> {
+    const existingSkill = await this.skillsRepository.findOneBy({
+      title: title,
+    });
+    if (!existingSkill) {
+      throw new HttpException("Skill not found", 404);
+    }
+    return this.skillsRepository.remove(existingSkill);
   }
 }
