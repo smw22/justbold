a67524125876ca63341a08a545b1e5fd3d1210db
BFalberg <benjaminfalberg@gmail.com>
2025-12-05 09:32:22 +0100

    add nav links with current state

--- frontend/app/components/AppWrapper.tsx
+++ frontend/app/components/AppWrapper.tsx
@@ -1,26 +1,13 @@
 import React from "react";
 import { useLocation, matchPath } from "react-router";
 
-export default function AppWrapper({
-  children,
-}: {
-  children: React.ReactNode;
-}) {
+export default function AppWrapper({ children }: { children: React.ReactNode }) {
   const location = useLocation();
 
   let bgColor: string;
-  const lightGreyPaths = [
-    "/collaborations",
-    "/services",
-    "/profile",
-    "/profile/:profileId",
-    "/profile/:profileId/edit",
-    "/create",
-  ];
+  const lightGreyPaths = ["/collaborations", "/services", "/profile", "/profile/:profileId", "/profile/:profileId/edit"];
 
-  const isLightGrey = lightGreyPaths.some((path) =>
-    matchPath({ path, end: true }, location.pathname)
-  );
+  const isLightGrey = lightGreyPaths.some((path) => matchPath({ path, end: true }, location.pathname));
 
   if (isLightGrey) {
     bgColor = "bg-light-grey";
