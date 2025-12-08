75021bb58fdc98703f5c3c744ef2bd9730421b2b
BFalberg <benjaminfalberg@gmail.com>
2025-12-08 16:41:50 +0100

    add likes endpoint to comments

--- backend/src/comments/comments.controller.ts
+++ backend/src/comments/comments.controller.ts
@@ -1,9 +1,10 @@
-import { Controller, Get, Post, Body, Patch, Param, Delete } from '@nestjs/common';
-import { CommentsService } from './comments.service';
-import { CreateCommentDto } from './dto/create-comment.dto';
-import { UpdateCommentDto } from './dto/update-comment.dto';
+import { Controller, Get, Post, Body, Patch, Param, Delete, Req } from "@nestjs/common";
+import { CommentsService } from "./comments.service";
+import { CreateCommentDto } from "./dto/create-comment.dto";
+import { UpdateCommentDto } from "./dto/update-comment.dto";
+import { Like } from "../likes/entities/like.entity";
 
-@Controller('comments')
+@Controller("comments")
 export class CommentsController {
   constructor(private readonly commentsService: CommentsService) {}
 
@@ -17,18 +18,59 @@ export class CommentsController {
     return this.commentsService.findAll();
   }
 
-  @Get(':id')
-  findOne(@Param('id') id: string) {
+  @Get(":id")
+  findOne(@Param("id") id: string) {
     return this.commentsService.findOne(+id);
   }
 
-  @Patch(':id')
-  update(@Param('id') id: string, @Body() updateCommentDto: UpdateCommentDto) {
+  @Patch(":id")
+  update(@Param("id") id: string, @Body() updateCommentDto: UpdateCommentDto) {
     return this.commentsService.update(+id, updateCommentDto);
   }
 
-  @Delete(':id')
-  remove(@Param('id') id: string) {
+  @Delete(":id")
+  remove(@Param("id") id: string) {
     return this.commentsService.remove(+id);
+  }
+
+  @Post(":id/likes")
+  async addLike(@Param("id") id: string, @Req() req) {
+    try {
+      const userId = req.user.id;
+      const data = await this.commentsService.addLike(id, userId);
+      // Fetch updated total likes
+      const likes = await this.commentsService.getLikes(id);
+      return {
+        success: true,
+        totalLikes: likes.length,
+        data,
+        message: "Like added successfully",
+      };
+    } catch (error) {
+      return {
+        success: false,
+        message: error.message,
+      };
+    }
+  }
+
+  @Delete(":id/likes")
+  async removeLike(@Param("id") id: string, @Req() req) {
+    try {
+      const userId = req.user.id;
+      const data = await this.commentsService.removeLike(id, userId);
+      const likes = await this.commentsService.getLikes(id);
+      return {
+        success: true,
+        totalLikes: likes.length,
+        data,
+        message: "Like removed successfully",
+      };
+    } catch (error) {
+      return {
+        success: false,
+        message: error.message,
+      };
+    }
   }
 }
--- backend/src/comments/comments.service.ts
+++ backend/src/comments/comments.service.ts
@@ -1,11 +1,22 @@
-import { Injectable } from '@nestjs/common';
-import { CreateCommentDto } from './dto/create-comment.dto';
-import { UpdateCommentDto } from './dto/update-comment.dto';
+import { Injectable, HttpException } from "@nestjs/common";
+import { InjectRepository } from "@nestjs/typeorm";
+import { Repository } from "typeorm";
+import { Like } from "../likes/entities/like.entity";
+import { Comment } from "./entities/comment.entity";
+import { CreateCommentDto } from "./dto/create-comment.dto";
+import { UpdateCommentDto } from "./dto/update-comment.dto";
 
 @Injectable()
 export class CommentsService {
+  constructor(
+    @InjectRepository(Comment)
+    private readonly commentsRepository: Repository<Comment>,
+    @InjectRepository(Like)
+    private readonly likesRepository: Repository<Like>
+  ) {}
+
   create(createCommentDto: CreateCommentDto) {
-    return 'This action adds a new comment';
+    return "This action adds a new comment";
   }
 
   findAll() {
@@ -22,5 +33,48 @@ export class CommentsService {
 
   remove(id: number) {
     return `This action removes a #${id} comment`;
+  }
+
+  async addLike(commentId: string, userId: string) {
+    const existingLike = await this.likesRepository.findOne({
+      where: {
+        type: "comment",
+        object_id: commentId,
+        user: { id: userId },
+      },
+      relations: ["user"],
+    });
+    if (existingLike) {
+      throw new HttpException("User has already liked this comment", 400);
+    }
+    const likeData = this.likesRepository.create({
+      type: "comment",
+      object_id: commentId,
+      user: { id: userId },
+    });
+    return this.likesRepository.save(likeData);
+  }
+
+  async removeLike(commentId: string, userId: string) {
+    const existingLike = await this.likesRepository.findOne({
+      where: {
+        type: "comment",
+        object_id: commentId,
+        user: { id: userId },
+      },
+      relations: ["user"],
+    });
+    if (!existingLike) {
+      throw new HttpException("Like not found", 404);
+    }
+    await this.likesRepository.remove(existingLike);
+    return { commentId, userId };
+  }
+
+  async getLikes(commentId: string) {
+    return this.likesRepository.find({
+      where: { type: "comment", object_id: commentId },
+      relations: ["user"],
+    });
   }
 }
