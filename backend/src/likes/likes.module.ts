3bbf32924ed9a58484ab8ec5113255ac74257aa1
Jacob Thorwest <jacob-jaytee@hotmail.com>
2025-12-08 11:13:25 +0100

    like functionality (middle of development)

--- backend/src/likes/likes.module.ts
+++ backend/src/likes/likes.module.ts
@@ -1,8 +1,11 @@
-import { Module } from '@nestjs/common';
-import { LikesService } from './likes.service';
-import { LikesController } from './likes.controller';
+import { Module } from "@nestjs/common";
+import { TypeOrmModule } from "@nestjs/typeorm";
+import { Like } from "./entities/like.entity";
+import { LikesService } from "./likes.service";
+import { LikesController } from "./likes.controller";
 
 @Module({
+  imports: [TypeOrmModule.forFeature([Like])], // <-- Add this line
   controllers: [LikesController],
   providers: [LikesService],
 })
