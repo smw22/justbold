622e63b30678771b32f2662755431783d4f5e9ee
BFalberg <benjaminfalberg@gmail.com>
2025-12-12 10:25:18 +0100

    micro style update

--- frontend/app/components/ErrorMessage.tsx
+++ frontend/app/components/ErrorMessage.tsx
@@ -2,7 +2,7 @@ import Icon from "./icon";
 export default function ErrorMessage({ error }: { error: string }) {
   return error ? (
     <div className="outer-wrapper">
-      <div className="flex gap-2 items-center border border-neutral-300 rounded-lg bg-neutral-100 p-2">
+      <div className="flex gap-2 items-center border border-red-400 rounded-lg bg-red-50 p-2 text-red-600">
         <Icon name="Help" size={24} />
         <p className="text-sm">{error}</p>
       </div>
