75e99556ca625cad64c2ef162baec2d044065000
BFalberg <benjaminfalberg@gmail.com>
2025-12-15 11:42:37 +0100

    update user dto

--- backend/src/users/dto/create-user.dto.ts
+++ backend/src/users/dto/create-user.dto.ts
@@ -1,10 +1,32 @@
+import { IsEmail, IsString, IsNumber, IsOptional, MinLength } from "class-validator";
+
 export class CreateUserDto {
+  @IsString()
   name: string;
+
+  @IsEmail()
   email: string;
+
+  @IsString()
+  @MinLength(6)
   password: string;
-  phone: string;
-  year_of_birth: number;
-  location: string;
-  subscription: string;
+
+  @IsString()
+  @IsOptional()
+  phone?: string;
+
+  @IsNumber()
+  @IsOptional()
+  year_of_birth?: number;
+
+  @IsString()
+  @IsOptional()
+  location?: string;
+
+  @IsString()
+  @IsOptional()
+  subscription?: string;
+
+  @IsString()
   user_type: string;
 }
